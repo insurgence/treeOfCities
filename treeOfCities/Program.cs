88ca47af51bc3e7d4a34dde6cafc0acefa11a823
vadim <insurgence@mail.ru>
2016-03-13 11:53:33 +0400

    First commit...

---
+++ treeOfCities/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace treeOfCities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\src.txt";
+            
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+            else
+            {
+                List<string> lines = File.ReadLines(path).ToList();
+
+                int x = 3;
+                Console.WriteLine($"Value of x = {x}");
+                
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
